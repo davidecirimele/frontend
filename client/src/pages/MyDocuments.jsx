eb321b742e58568e7137b3ba22618240f5d1b706
davidecirimele <davideciri7@gmail.com>
2023-01-27 17:07:40 +0100

    My documents page working

--- client/src/pages/MyDocuments.jsx
+++ client/src/pages/MyDocuments.jsx
@@ -1,9 +1,31 @@
 import React from "react";
 import "./MyDocuments.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 
-function myDocuments() {
+async function fetchData(){
+  const getToken = sessionStorage.getItem("token");
+    const documents = [];
+     await axios.get('http://127.0.0.1:8000/api/document/', {headers: {
+      'Authorization': `Token ${getToken}`,
+      },
+    }).then(result => {
+    console.log(result.data);
+    result.data.forEach(document => {
+      documents.push(document);
+    });
+    })
+    return documents;
+}
+
+const MyDocuments = () => {
+  const [documents, setDocument] = useState([]);
+  
+  useEffect(() => {
+    fetchData().then(result => setDocument(result));
+  }, []);
+
   return (
     <div className="home">
       {/* <HeaderBar />
@@ -12,11 +34,24 @@ function myDocuments() {
             <LeftSide />
         </div>  */}
       <div className="main-content">
-        <h1>this is my documments page</h1>
+        <h1>this is my documents page</h1>
       </div>
-      {/* </div> */}
+      <div className="documents-container">
+                {documents?.map(document => (
+                    <div key={document.id_number} className="document-card">
+                        <div className="left-side">
+                            <img src = { document.document_img } className="document-icon"  width="20%" height="20%"/>
+                        </div>
+                        <div className="rightside">
+                            <div className="document-type">
+                                {document.type}
+                            </div>  
+                        </div>
+                    </div>
+                ))}
+            </div>
     </div>
   );
 }
 
-export default myDocuments;
+export default MyDocuments;
