9770bb95ba397001a63ad6fe56c02aa4e28f4449
davidecirimele <davideciri7@gmail.com>
2023-01-29 02:05:57 +0100

    Notifications page updated

--- client/src/pages/MyDocuments.jsx
+++ client/src/pages/MyDocuments.jsx
@@ -3,8 +3,10 @@ import "./MyDocuments.css";
 import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import axios from "axios";
+import { color } from "@mui/system";
 
-async function fetchData(){
+
+async function fetchDocuments(){
   const getToken = sessionStorage.getItem("token");
     const documents = [];
      await axios.get('http://127.0.0.1:8000/api/document/', {headers: {
@@ -19,12 +21,53 @@ async function fetchData(){
     return documents;
 }
 
+async function fetchDegrees(){
+  const getToken = sessionStorage.getItem("token");
+    const degrees = [];
+     await axios.get('http://127.0.0.1:8000/api/degree/', {headers: {
+      'Authorization': `Token ${getToken}`,
+      },
+    }).then(result => {
+    console.log(result.data);
+    result.data.forEach(degree => {
+      degrees.push(degree);
+    });
+    })
+    return degrees;
+}
+
+function checkValidity(document){
+  const currentDate = new Date();
+  const expirationDate = new Date(document.date_of_expiration);
+
+  if (currentDate <= expirationDate) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function addDocument(){
+  window.location.href = '/document';
+}
+
+function addQualification(){
+  window.location.href = '/degree';
+}
+
 const MyDocuments = () => {
   const [documents, setDocument] = useState([]);
+  const [degrees, setDegree] = useState([]);
+  
   
   useEffect(() => {
-    fetchData().then(result => setDocument(result));
+    fetchDocuments().then(result => setDocument(result));
   }, []);
+  useEffect(() => {
+    fetchDegrees().then(result => setDegree(result));
+  }, []);
+
+
 
   return (
     <div className="home">
@@ -34,7 +77,14 @@ const MyDocuments = () => {
             <LeftSide />
         </div>  */}
       <div className="main-content">
-        <h1>this is my documents page</h1>
+        <h1>My Documents</h1>
+      </div>
+      <div className = "button-div">
+        <button type="button" onClick = {addDocument}>Add a Document</button>
+        <button type="button" onClick = {addQualification}>Add a Qualification</button>
+      </div>
+      <div className="title-content">
+        <h1>ID Documents</h1>
       </div>
       <div className="documents-container">
                 {documents?.map(document => (
@@ -44,13 +94,38 @@ const MyDocuments = () => {
                         </div>
                         <div className="rightside">
                             <div className="document-type">
-                                {document.type}
-                            </div>  
+                                Document: {document.type}
+                            </div> 
+                            <div className="document-number">
+                                Id Number: {document.id_number}
+                            </div> 
+                            {checkValidity(document)?<div className="green">valid</div> : <div className="red">expired</div>}
+                        </div>
+                    </div>
+                ))}
+            </div>
+          <div className="title-content">
+            <h1>Qualifications</h1>
+          </div>
+          <div className="degree-container">
+                {degrees?.map(degree => (
+                    <div key={degree.discipline} className="degree-card">
+                        <div className="left-side">
+                            <img src = '/icons/degree.png' className="degree-icon"  width="20%" height="20%" />
+                        </div>
+                        <div className="rightside">
+                            <div className="degree-type">
+                                Grade: {degree.type_of_degree}
+                            </div> 
+                            <div className="degree-discipline">
+                                Discipline: {degree.discipline}
+                            </div> 
                         </div>
                     </div>
                 ))}
             </div>
     </div>
+    
   );
 }
 
--- client/src/pages/MyNotification.jsx
+++ client/src/pages/MyNotification.jsx
@@ -1,14 +1,55 @@
 import React from "react";
+import axios from "axios";
+import "./MyNotification.css";
+import { useState, useEffect } from "react";
+
+async function fetchNotifications(){
+  const getToken = sessionStorage.getItem("token");
+    const notifications = [];
+     await axios.get('http://127.0.0.1:8000/api/notification/', {headers: {
+      'Authorization': `Token ${getToken}`,
+      },
+    }).then(result => {
+    console.log(result.data);
+    result.data.forEach(notification => {
+      notifications.push(notification);
+    });
+    })
+    return notifications;
+}
 
 
 function MyNotification() {
+  const [notifications, setNotification] = useState([]);
+  
+  useEffect(() => {
+    fetchNotifications().then(result => setNotification(result));
+  }, []);
+
   return (
     <div className="home">
       {/* <div className="sidebar">
         <LeftSide />
       </div>  */}
       <div className="homeContainer">
-        <h1>this is my notifications page</h1>
+        <h1>Notifications</h1>
+        <div className="notifications-container">
+                {notifications?.map(notification => (
+                    <div key={notification.title} className="notification-card">
+                        <div className="left-side">
+                            <img src = '/icons/red_icon_notification.png' className="document-icon"  width="5%" height="5%"/>
+                        </div>
+                        <div className="rightside">
+                            <div className="notification-title">
+                                Title: {notification.title}
+                            </div> 
+                            <div className="notification-message">
+                                Message: {notification.body}
+                            </div> 
+                        </div>
+                    </div>
+                ))}
+            </div>
       </div>
     </div>
   );
